e5eca1bc6cca4a632a5b638a713e2e37b622056a
Levi Ackerman <umeshshelare@gmail.com>
2024-05-07 10:36:52 +0530

    app that convert kilo into grams

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.unitconverter;
+
+import android.health.connect.datatypes.units.Length;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView gramas;
+    Button convertintograms;
+    EditText weightintkilo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeViews();
+
+        convertintograms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                convertintograms();
+            }
+        });
+
+
+    }
+
+    private void convertintograms() {
+        float kilo=0,grams=0;
+
+        kilo= Float.parseFloat(weightintkilo.getText().toString());
+
+//        Toast.makeText(this,""+weightintkilo.getText(),Toast.LENGTH_LONG).show();
+
+        grams=kilo*1000;
+
+        gramas.setText(""+grams+" grams");
+
+
+    }
+
+    private void initializeViews() {
+        gramas = findViewById(R.id.grams);
+        convertintograms = findViewById(R.id.converter);
+        weightintkilo=findViewById(R.id.weight_kilo);
+
+    }
+}
